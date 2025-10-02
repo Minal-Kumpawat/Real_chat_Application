e5fb9b28e11da01875b0a6f1f2a68c248c782e04
Minal Kumpawat <minalkumpawat@gmail.com>
2025-10-02 12:11:59 +0530

    real chat application

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 2563;
+
+// create socket.io server
+const io = require("socket.io")(8000, {
+    cors: {
+        origin: "*",
+    }
+});
+
+// serve frontend files
+app.use(express.static(path.join(__dirname, "public")));
+
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user-join', name => {
+        console.log("new user --", name);
+        users[socket.id] = name;
+
+        // broadcast to others
+        socket.broadcast.emit('user-join', name);
+
+        // also send notification
+        socket.broadcast.emit('notification', {type: "join", text: `${name} joined the chat`});
+    });
+
+    socket.on('send', message => {
+        // send message to others
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
+
+        // send notification
+        socket.broadcast.emit('notification', {type: "message", text: `${users[socket.id]} sent: ${message}`});
+    });
+
+    socket.on('disconnect', () => {
+        if (users[socket.id]) {
+            socket.broadcast.emit('left', `${users[socket.id]} left the chat`);
+
+            // also send notification
+            socket.broadcast.emit('notification', {type: "leave", text: `${users[socket.id]} left the chat`});
+
+            delete users[socket.id];
+        }
+    });
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
